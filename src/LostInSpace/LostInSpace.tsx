e35bd95cc1578c8c20bbb4efe0e5e6e396d0c365
Todd Elvers <todd.elvers@equipmentshare.com>
2020-11-29 18:16:26 -0600

    Corrected jest coverage settings

--- src/LostInSpace/LostInSpace.tsx
+++ src/LostInSpace/LostInSpace.tsx
@@ -1,3 +1,5 @@
+/* istanbul ignore file */
+
 import * as React from 'react';
 
 import './LostInSpace.css';
@@ -7,71 +9,83 @@ type Props = {
   readonly width?: React.CSSProperties['width'];
 };
 
-const drawVisor = (visorRef: React.RefObject<HTMLCanvasElement>) => {
-  const ctx = visorRef.current?.getContext('2d');
-  if (!ctx) return;
+/* istanbul ignore next */
+const useAstronautCord = () => {
+  const cordRef = React.createRef<HTMLCanvasElement>();
+  let y1 = 160;
+  let y2 = 100;
+  let y3 = 100;
 
-  ctx.beginPath();
-  ctx.moveTo(5, 45);
-  ctx.bezierCurveTo(15, 64, 45, 64, 55, 45);
+  let y1Forward = true;
+  let y2Forward = false;
+  let y3Forward = true;
 
-  ctx.lineTo(55, 20);
-  ctx.bezierCurveTo(55, 15, 50, 10, 45, 10);
+  const animate = () => {
+    const ctx = cordRef.current?.getContext('2d');
+    if (!ctx) return;
 
-  ctx.lineTo(15, 10);
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-  ctx.bezierCurveTo(15, 10, 5, 10, 5, 20);
-  ctx.lineTo(5, 45);
+    ctx.beginPath();
+    ctx.moveTo(130, 170);
+    ctx.bezierCurveTo(250, y1, 345, y2, 400, y3);
 
-  ctx.fillStyle = '#2f3640';
-  ctx.strokeStyle = '#f5f6fa';
-  ctx.fill();
-  ctx.stroke();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 8;
+    ctx.stroke();
+
+    if (y1 === 100) y1Forward = true;
+    if (y1 === 300) y1Forward = false;
+    if (y2 === 100) y2Forward = true;
+    if (y2 === 310) y2Forward = false;
+    if (y3 === 100) y3Forward = true;
+    if (y3 === 317) y3Forward = false;
+
+    y1Forward ? (y1 += 1) : (y1 -= 1);
+    y2Forward ? (y2 += 1) : (y2 -= 1);
+    y3Forward ? (y3 += 1) : (y3 -= 1);
+  };
+
+  React.useEffect(() => {
+    animate();
+  }, [cordRef?.current]);
+
+  return cordRef;
+};
+
+/* istanbul ignore next */
+const useAstronautVisor = () => {
+  const visorRef = React.createRef<HTMLCanvasElement>();
+
+  React.useEffect(() => {
+    const ctx = visorRef.current?.getContext('2d');
+    if (!ctx) return;
+
+    ctx.beginPath();
+    ctx.moveTo(5, 45);
+    ctx.bezierCurveTo(15, 64, 45, 64, 55, 45);
+
+    ctx.lineTo(55, 20);
+    ctx.bezierCurveTo(55, 15, 50, 10, 45, 10);
+
+    ctx.lineTo(15, 10);
+
+    ctx.bezierCurveTo(15, 10, 5, 10, 5, 20);
+    ctx.lineTo(5, 45);
+
+    ctx.fillStyle = '#2f3640';
+    ctx.strokeStyle = '#f5f6fa';
+    ctx.fill();
+    ctx.stroke();
+  }, [visorRef]);
+
+  return visorRef;
 };
 
 export const LostInSpace = ({ height = '100vh', width = '100wh' }: Props) => {
-  const visorRef = React.createRef<HTMLCanvasElement>();
-  const cordRef = React.createRef<HTMLCanvasElement>();
-
-  React.useEffect(() => {
-    let y1 = 160;
-    let y2 = 100;
-    let y3 = 100;
-
-    let y1Forward = true;
-    let y2Forward = false;
-    let y3Forward = true;
-
-    const animate = () => {
-      const ctx = cordRef.current?.getContext('2d');
-      if (!ctx) return;
-
-      requestAnimationFrame(animate);
-      ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-      ctx.beginPath();
-      ctx.moveTo(130, 170);
-      ctx.bezierCurveTo(250, y1, 345, y2, 400, y3);
-
-      ctx.strokeStyle = 'white';
-      ctx.lineWidth = 8;
-      ctx.stroke();
-
-      if (y1 === 100) y1Forward = true;
-      if (y1 === 300) y1Forward = false;
-      if (y2 === 100) y2Forward = true;
-      if (y2 === 310) y2Forward = false;
-      if (y3 === 100) y3Forward = true;
-      if (y3 === 317) y3Forward = false;
-
-      y1Forward ? (y1 += 1) : (y1 -= 1);
-      y2Forward ? (y2 += 1) : (y2 -= 1);
-      y3Forward ? (y3 += 1) : (y3 -= 1);
-    };
-
-    drawVisor(visorRef);
-    animate();
-  }, [visorRef, cordRef]);
+  const visorRef = useAstronautVisor();
+  const cordRef = useAstronautCord();
 
   return (
     <div className="moon__container" style={{ height, width }}>
